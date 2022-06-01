6ada6022045a5405299e0cb8747cf3d59a2de72a
sohailul <sohailulalam@gmail.com>
2022-06-02 00:12:24 +0600

    initial setup Navbar and other component created

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        sfebdtheme: {
+          primary: "#FF4F00",
+          secondary: "#fdba74",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
